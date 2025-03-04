8143aad29a07b5a24b91b3c10a85e7bb11fddaf5
GMarsura <biel.marsura@gmail.com>
2025-03-04 17:09:16 -0300

    projeto biografia alan turing - pam 2025

---
+++ bat_biogafia_alan_turing/src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text>Project PAM Alan Turing- Thiago Moreira, Gabriel Ortiz</Text>
+    </View>
+  );
+}
