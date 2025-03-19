8dfd3ab17b6fa0cb146563f4a3bff9936d49309f
GMarsura <biel.marsura@gmail.com>
2025-03-19 20:23:47 -0300

    change medium

--- medium/src/app/_layout.tsx
+++ medium/src/app/_layout.tsx
@@ -1,6 +1,5 @@
-import '../styles/global.css'
-import { Slot } from "expo-router";
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Slot/>;
+  return <Stack />;
 }
--- medium/tailwind.config.js
+++ medium/tailwind.config.js
@@ -1,17 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
     // NOTE: Update this to include the paths to all of your component files.
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     presets: [require("nativewind/preset")],
     theme: {
-      extend: {
-        fontFamily: {
-          yrsa: ['"Yrsa"', "serif"],
-          unna: ['"Unna"', "serif"],
-          sometypeMono: ['"Sometype Mono"', "monospace"],
-        }
-      },
+      extend: {},
     },
     plugins: [],
   }
